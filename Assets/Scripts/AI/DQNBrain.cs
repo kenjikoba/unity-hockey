ad46f6d863cc4c68cb99d934b70b8b94ff43aa4e
kenjikoba <kenji.kobayashi0419@gmail.com>
2022-11-08 21:08:30 +0900

    Created DQNfiles

---
+++ Assets/Scripts/AI/DQNBrain.cs
@@ -0,0 +1,141 @@
+﻿using UnityEngine; // Load関数が少し違うのと、Save関数が本当に絶妙に違う。
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System;
+
+public class DQNBrain : Brain
+{
+    public int StateSize { get; private set; }
+    public int ActionSize { get; private set; }
+
+    [SerializeField] private float epsilon = 1.0f;
+    [SerializeField] private float epsilonMin;
+
+    private float[][] QTable { get; set; }
+    // Initial epsilon value for random action selection.}
+    private float Epsilon { get; set; } = 1.0f;
+    // Lower bound of epsilon.
+    private float EpsilonMin { get; set; } = 0.0f;
+    // Number of steps to lower e to eMin.
+    private int AnnealingSteps { get; set; } = 100000;
+    // Discount factor for calculating Q-target.
+    private float Gamma { get; set; } = 0.75f;
+    // The rate at which to update the value estimates given a reward.
+    private float ETA { get; set; } = 0.1f;
+
+    public void CreateTable()
+    {
+        Epsilon = epsilon;
+        EpsilonMin = epsilonMin;
+
+        QTable = new float[StateSize][];
+        for (int i = 0; i < StateSize; i++)
+        {
+            QTable[i] = new float[ActionSize]; // 初期化で最初は0が入ってるっぽい。
+        }
+        //Load("QT01.txt");
+    }
+
+    public DQNBrain(int stateSize, int actionSize)
+	{
+		StateSize = stateSize;
+		ActionSize = actionSize;
+		CreateTable();
+	}
+
+    public int GetAction(int state)
+    {
+        int action;
+
+        if (Epsilon <= UnityEngine.Random.Range(0.0f, 1.0f))
+        {
+            action = QTable[state].ToList().IndexOf(QTable[state].Max());
+        }
+        else
+        {
+            action = UnityEngine.Random.Range(0, ActionSize);
+        }
+
+        if (Epsilon > EpsilonMin)
+        {
+            Epsilon = Epsilon - ((1f - EpsilonMin) / AnnealingSteps);
+        }
+
+        return action;
+    }
+
+    public float[] GetValue()
+    {
+        float[] value_table = new float[QTable.Length];
+        for (int i = 0; i < QTable.Length; i++)
+        {
+            value_table[i] = QTable[i].Average();
+        }
+        return value_table;
+    }
+
+    public void UpdateTable(int lastState, int nextState, int action, float reward, bool done)
+    {
+        if (action != -1)
+        {
+            if (done == true)
+            {
+                QTable[lastState][action] += ETA * (reward - QTable[lastState][action]);
+            }
+            else
+            {
+                if (nextState != lastState)
+                {
+                    QTable[lastState][action] += ETA * (reward + Gamma * QTable[nextState].Max() - QTable[lastState][action]);
+                }
+                else
+                {
+                    QTable[lastState][action] += ETA * (reward - QTable[lastState][action]); //?
+                }
+            }
+        }
+    }
+
+// path, FileMode.OpenOrCreate
+// path, FileMode.Create, FileAccess.Write
+    public override void Save(string path)
+    {
+        using (var bw = new BinaryWriter(new FileStream(path, FileMode.OpenOrCreate)))
+        {
+            for (int i = 0; i < StateSize; i++)
+            {
+                for (int j = 0; j < ActionSize; j++)
+                {
+                    bw.Write(QTable[i][j]);
+                }
+            }
+        }
+    }
+
+    public override void Load(string path)
+    {
+        int size = 60;
+        using (var br = new BinaryReader(new FileStream(path, FileMode.Open)))
+        {
+            for (int i = 0; i < StateSize / size; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    QTable[i][j] = br.ReadSingle();
+                }
+            }
+        }
+
+        for (int i = 1; i < size; i++)
+        {
+            for (int j = 0; j < StateSize / size; j++)
+            {
+                for (int k = 0; k < 3; k++)
+                {
+                    QTable[StateSize / size * i + j][k] = QTable[j][k] * 0.1f;
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/AI/DQNEnvironment.cs
@@ -0,0 +1,183 @@
+﻿// Q学習、基本的に変わりはEpisode以外あんまない。
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+
+public class DQNEnvironment : Environment
+{
+//     // 加えた。
+    /***** Num of Symaltaneously Playing Agents *****/
+    [SerializeField] private int nAgents = 2;
+    private int NAgents { get { return nAgents; } }
+    
+    // /***** SerializeField for gameObjects *****/
+    [Header("Agent Prefab"), SerializeField] private GameObject GObject1;
+    [Header("Agent Prefab"), SerializeField] private GameObject GObject2;
+
+    [SerializeField] private int actionSize = 6;
+    private int ActionSize { get { return actionSize; } }
+
+    [SerializeField] private int stateSize = 1024;
+    private int StateSize { get { return stateSize; } }
+
+    private DQNBrain DQNBrain1;
+    private DQNBrain DQNBrain2;
+    private HockeyAgent LearningAgent1;
+    private HockeyAgent LearningAgent2;
+
+    private int PrevState1;
+    private int PrevState2;
+
+    /***** Values for Record *****/
+    private float BestRecord { get; set; }
+
+    /***** Bool Objects For Snchronization With Opponent Player *****/
+    // Getter and Setter should be written?
+    public bool WaitingFlag = false;
+    public bool RestartFlag = false;
+    public bool ManualModeFlag = false;
+
+    [Header("UI References"), SerializeField] private Text Qtext = null;
+    
+    /***** Property of GameObjects *****/
+    private Text QText { get { return Qtext; } }
+
+    /***** 学習が開始された際に呼ばれる *****/
+    void Awake() {
+        // 同時にプレイするプレーヤー数. 現在は2で固定.
+        if (nAgents != 2) {
+            Debug.Log("Now, nAgents must be equal to 2.");
+            nAgents = 2;
+        }
+        
+        DQNBrain1 = new DQNBrain(StateSize, ActionSize);
+        DQNBrain2 = new DQNBrain(StateSize, ActionSize);
+        LearningAgent1 = GObject1.GetComponent<HockeyAgent>();
+        LearningAgent2 = GObject2.GetComponent<HockeyAgent>();
+        PrevState1 = LearningAgent1.GetState();
+        PrevState2 = LearningAgent2.GetState();
+    }
+
+    public void Inactivate() {
+        ManualModeFlag = true;
+        GObject1.SetActive(false);
+        GObject2.SetActive(false);
+    }
+
+    public void Activate() {
+        ManualModeFlag = false;
+        GObject1.SetActive(true);
+        GObject2.SetActive(true);
+    }
+
+    // Agentが切り替わる際にReset()が呼ばれる
+    /***** Reset() Is Used When Agents Change *****/
+    public void Reset() {
+        WaitingFlag = false;
+    }
+
+    // Agentが変わらない場合はRestart()が呼ばれる
+    /***** Restart() Is Used When Agents Don't Change *****/
+    public void Restart() {
+        RestartFlag = false;
+        LearningAgent1.TimeUp = false;
+        LearningAgent2.TimeUp = false;
+        // AgentsSet.ForEach(p => { p.agent.TimeUp = false; });
+    }
+
+    private void FixedUpdate()
+    {
+        // Waiting, Restart, ManualModeのいずれかがtrueであれば学習を進めない
+        if (WaitingFlag || RestartFlag || ManualModeFlag) {
+            return;
+        }
+
+        // if (LearningAgent1.IsDone) {
+        //     int r = (int)LearningAgent1.Reward;
+        //     BestRecord = Mathf.Max(r, BestRecord);
+        //     System.Console.WriteLine("Hello, World! ={0} \n",BestRecord.ToString());
+        //     File.WriteAllText("./Assets/BestRecord.txt", BestRecord.ToString());
+        //     QLBrain1.Save("./Assets/Q.txt");
+        //     // WaitingFlag = true;
+        // }
+        if (LearningAgent1.TimeUp) {
+            RestartFlag = true;
+            int r = (int)LearningAgent1.Reward;
+            BestRecord = Mathf.Max(r, BestRecord);
+            int pastRecord = 0;
+            // インスタンスを作成，パスをコンストラクタに渡す
+            using (var objReader = new StreamReader("./Assets/BestRecord.txt")) {
+                string pastRead = objReader.ReadToEnd();
+                pastRecord = int.Parse(pastRead);
+            }
+            if (pastRecord < BestRecord) {
+                File.WriteAllText("./Assets/BestRecord.txt", BestRecord.ToString());
+                DQNBrain1.Save("./Assets/StreamingAssets/ComputerBrains/Q.txt");
+            }
+            UpdateText();
+        }
+        // if (LearningAgent2.IsDone) {
+        //     int r = (int)LearningAgent2.Reward;
+        //     BestRecord = Mathf.Max(r, BestRecord);
+        //     System.Console.WriteLine("Hello, World! ={0} \n",BestRecord.ToString());
+        //     File.WriteAllText("./Assets/BestRecord.txt", BestRecord.ToString());
+        //     QLBrain2.Save("./Assets/Q.txt");
+        //     // WaitingFlag = true;
+        // }
+        if (LearningAgent2.TimeUp) {
+            RestartFlag = true;
+            int r = (int)LearningAgent2.Reward;
+            BestRecord = Mathf.Max(r, BestRecord);
+            int pastRecord = 0;
+            // インスタンスを作成，パスをコンストラクタに渡す
+            using (var objReader = new StreamReader("./Assets/BestRecord.txt")) {
+                string pastRead = objReader.ReadToEnd();
+                pastRecord = int.Parse(pastRead);
+            }
+            if (pastRecord < BestRecord) {
+                File.WriteAllText("./Assets/BestRecord.txt", BestRecord.ToString());
+                DQNBrain2.Save("./Assets/StreamingAssets/ComputerBrains/Q.txt");
+            }
+            UpdateText();
+        }
+
+        AgentUpdate(LearningAgent1, DQNBrain1,1);
+        if (LearningAgent1.IsDone)
+        {
+            LearningAgent1.Reset();
+        }
+        AgentUpdate(LearningAgent2, DQNBrain2,2);
+        if (LearningAgent2.IsDone)
+        {
+            LearningAgent2.Reset();
+        }
+    }
+
+    private void AgentUpdate(Agent a, DQNBrain b, int num)
+    {
+        if (num == 1) {
+            int ActionNo = b.GetAction(PrevState1); 
+            var action = a.ActionNumberToVectorAction(ActionNo);
+            a.AgentAction(action);
+            var NewState = a.GetState();
+            b.UpdateTable(PrevState1, NewState, ActionNo, a.Reward, a.IsDone);
+            PrevState1 = NewState;
+        }
+        else {
+            int ActionNo = b.GetAction(PrevState2); 
+            var action = a.ActionNumberToVectorAction(ActionNo);
+            a.AgentAction(action);
+            var NewState = a.GetState();
+            b.UpdateTable(PrevState2, NewState, ActionNo, a.Reward, a.IsDone);
+            PrevState2 = NewState;
+        }
+    }
+
+    private void UpdateText() {
+        QText.text = "Best Record: " + BestRecord; 
+    }
+}
